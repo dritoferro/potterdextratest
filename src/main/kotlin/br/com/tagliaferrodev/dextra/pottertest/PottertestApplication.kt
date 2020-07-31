1122e92e6844dbc3b618c1bf0628480dfe39de69
Adriano <adrianotagliaferro@hotmail.com>
2020-07-31 14:43:45 -0300

    Initial commit.

---
+++ src/main/kotlin/br/com/tagliaferrodev/dextra/pottertest/PottertestApplication.kt
@@ -0,0 +1,11 @@
+package br.com.tagliaferrodev.dextra.pottertest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PottertestApplication
+
+fun main(args: Array<String>) {
+    runApplication<PottertestApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/tagliaferrodev/dextra/pottertest/PottertestApplicationTests.kt
@@ -0,0 +1,8 @@
+package br.com.tagliaferrodev.dextra.pottertest
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PottertestApplicationTests {
+
+}
