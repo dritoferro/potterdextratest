dab36f8cd61f2b7ba2e7463b211fff4a32e88d84
Adriano Augusto Tagliaferro <adrianotagliaferro@hotmail.com>
2020-08-01 16:38:12 -0300

    Feature/create character (#14)
    * Created endpoint to persist character data.
    
    * Updated docker-compose.yml to map volume for postgreSQL not to lose data.
    
    * Created mocked tests.

--- src/main/kotlin/br/com/tagliaferrodev/dextra/pottertest/character/CharacterService.kt
+++ src/main/kotlin/br/com/tagliaferrodev/dextra/pottertest/character/CharacterService.kt
@@ -1,7 +1,16 @@
 package br.com.tagliaferrodev.dextra.pottertest.character
 
+import br.com.tagliaferrodev.dextra.pottertest.character.domain.Character
+import br.com.tagliaferrodev.dextra.pottertest.integration.PotterClient
 import org.springframework.stereotype.Service
 
 @Service
-class CharacterService {
+class CharacterService(private val repository: CharacterRepository,
+                       private val partner: PotterClient) {
+
+    fun save(character: Character): Character {
+        partner.findByHouseId(character.house!!)
+
+        return repository.save(character)
+    }
 }
