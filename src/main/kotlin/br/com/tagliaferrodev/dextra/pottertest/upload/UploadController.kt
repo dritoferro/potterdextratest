d1b8a1cdbc144863c7f4efdf0b82be9137364869
Adriano <adrianotagliaferro@hotmail.com>
2020-10-18 10:18:50 -0300

    refactor: fix broken route

--- src/main/kotlin/br/com/tagliaferrodev/dextra/pottertest/upload/UploadController.kt
+++ src/main/kotlin/br/com/tagliaferrodev/dextra/pottertest/upload/UploadController.kt
@@ -2,15 +2,17 @@ package br.com.tagliaferrodev.dextra.pottertest.upload
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
-@RestController("uploads")
+@RestController
+@RequestMapping("uploads")
 class UploadController {
 
     @PostMapping("image")
     fun uploadFile(@RequestParam("arquivo") file: MultipartFile): ResponseEntity<String> {
-        return ResponseEntity.ok("Recebido o arquivo ${file.name}")
+        return ResponseEntity.ok("Recebido o arquivo ${file.originalFilename}")
     }
 }
